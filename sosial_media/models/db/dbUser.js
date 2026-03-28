e7dffa1b85256430260f0d6f53433d9024e9676f
lee0702 <happy1ii61i4eee@gmail.com>
2026-03-28 20:56:58 +0800

    fix: ignore docker data and move package.json to root

--- sosial_media/models/db/dbUser.js
+++
@@ -1,9 +0,0 @@
-const postgres = require('postgres');
-let pgConfig;
-if (!process.env.TEST_MODE){
-    pgConfig=require('../../configs/postgres.json');
-    console.log('Connect to production database.');
-} else {
-    pgConfig=require('../../configs/postgres-test.json');
-    console.log('Connect to test database.');
-}
